ada72b52a72e452a17e8934b282e46593c580a51
Vladimr Kondratsenko <vovanych1981@gmail.com>
2023-04-06 18:29:49 +0300

    4

--- StudentCppRevisionTask06Project/task03.cpp
+++ StudentCppRevisionTask06Project/task03.cpp
@@ -22,5 +22,15 @@
 */
 
 bool task03(int x1, int y1, int x2, int y2) {
-	return false;
+
+	if (x1 <= 0  x1 > 8  y1 <= 0  y1 > 8  x2 <= 0  x2 > 8  y2 <= 0 || y2 > 8) {
+		return false;
+	}
+
+	if ((x1 == x2 && y1 != y2) || (x1 != x2 && y1 == y2)) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
--- StudentCppRevisionTask06Project/task04.cpp
+++ StudentCppRevisionTask06Project/task04.cpp
@@ -22,5 +22,12 @@
 */
 
 bool task04(int x1, int y1, int x2, int y2) {
-	return false;
-}
+
+	if (x1 > 8 || x2 > 8 || y1 > 8 || y2 > 8  || x1 <= 0 || x2 <= 0 || y1 <= 0 || y2 <= 0) {
+		return false;
+	}
+	if (x1 + 1 == x2 || x1 - 1 == x2 || y1 + 1 == y2 || y1 - 1 == y2 ||  x1 + 1 == x2 && y1 + 1 == y2 || x1 - 1 == x2 && y1 - 1 == y2||
+		x1 + 1 == x2 && y1 - 1 == y2 || x1 - 1 == x2 && y1 + 1 == y2) {
+		return true;
+	} return false;
+
